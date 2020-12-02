1856c15490aff7c15934c12b9841735f045892f0
Uriel Florez <urielflorez29@gmail.com>
2020-12-02 18:26:24 -0500

    union en una sola carpeta

---
+++ mi_formulario/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Routing\Route as RoutingRoute;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('formulario', function () {
+    return 'Estas en el formulario';
+});
+Route::view('form', 'formulario');
